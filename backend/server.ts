c09c8763bcaa2c2ca6dfa77d634514a73652bb6b
rento yabuki <rento.nky.1203@gmail.com>
2025-12-14 17:32:44 +0900

    style(004): Format code for better readability in server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,22 +1,22 @@
 import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
 
 const server: FastifyInstance = Fastify({
-	logger: true
+  logger: true
 })
 
 const opts: RouteShorthandOptions = {
-	schema: {
-		response: {
-			200: {
-				type: 'object',
-				properties: {
-					pong: {
-						type: 'string'
-					}
-				}
-			}
-		}
-	}
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          pong: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
 }
 
 server.get('/health', async () => {
@@ -28,16 +28,16 @@ server.get('/health', async () => {
 })
 
 server.get('/ping', opts, async () => {
-	return { pong: 'it worked!' }
+  return { pong: 'it worked!' }
 })
 
 const start = async () => {
-	try {
-		await server.listen({ port:3000, host: '0.0.0.0' })
-	} catch (err) {
-		server.log.error(err)
-		process.exit(1)
-	}
+  try {
+    await server.listen({ port:3000, host: '0.0.0.0' })
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
 }
 
 start()
